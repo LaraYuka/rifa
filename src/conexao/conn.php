6325eb60a6203eaeb74fafbd459fb19d92d3f9d0
larayuka <larayuka.sak@gmail.com>
2022-05-22 22:12:38 -0300

    add dessistindo

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -2,7 +2,7 @@
 
     //Variaveis para realização da conexão com o banco de dados
     $hostname = "sql102.epizy.com";
-    $database = "epiz_31454906_BD_RIFA";
+    $dbname = "epiz_31454906_BD_RIFA";
     $username = "epiz_31454906";
     $password = "LrJmupbM3lTgBD";
 
